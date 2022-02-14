811cf4369112a7effb09cd991b9e256b324eb12f
Jiangjunyu <double.chiang@gmail.com>
2022-02-14 14:32:22 -0800

    fixes #18, use YAML to config the settings with Hop and Test Station

--- qmfnetop.py
+++ qmfnetop.py
@@ -3,11 +3,11 @@ import subprocess
 import os
 import threading
 import logging
+import yaml
 
 
 class QMFNetOp:
-    Station  = 'root@192.168.0.81 root@192.168.0.82 root@192.168.0.83 root@192.168.0.84 root@192.168.0.101 root@192.168.0.123'.split()
-    hopStation ='cchiang@192.168.66.28'
+    SETTTINGS_FILE='settings.yml'
     """ QMF network operation
     """
     def querySn(self, sn):
@@ -17,7 +17,7 @@ class QMFNetOp:
         # ls -1rt
         # -t     sort by modification time
         cmd = "find /RACKLOG/ -type f -iname *{}* -exec ls -tlhgG --time-style=long-iso {{}} +".format(sn)
-        for ip in QMFNetOp.Station:
+        for ip in self.ts:
             x=threading.Thread(target=self.remoteJob, args=(found, ip, cmd, error))
             threads.append(x)
             x.start()
@@ -41,7 +41,9 @@ class QMFNetOp:
         # .replace('(', '\(').replace(')', '\)')
         fn = os.path.basename(path)
         # Copy file to hop station 
-        cmd = "scp -o ProxyCommand=\"ssh -W %h:%p {}\" {}:'{}' /tmp".format(QMFNetOp.hopStation, ip, path)
+        cmd = "scp {}:'{}' /tmp".format(ip, path)
+        if self.hopStation is not None:
+            cmd = "scp -o ProxyCommand=\"ssh -W %h:%p {}\" {}:'{}' /tmp".format(self.hopStation, ip, path)
         logging.debug(cmd)
 
         # using subprocess run will have problem when copy filename with brace.
@@ -55,7 +57,8 @@ class QMFNetOp:
         """ Execute the cmd on the remote server
         """
         hopcmd = self.__sshHop(cmd, '{}'.format(ip))
-        hopcmd = self.__sshHop(hopcmd, QMFNetOp.hopStation)
+        if self.hop is not None:
+            hopcmd = self.__sshHop(hopcmd, self.hop)
         logging.info(subprocess.__file__)
         logging.debug(hopcmd)
         error[ip] = "searched"
@@ -83,12 +86,27 @@ class QMFNetOp:
     def __sshHop(self, cmd, hop):
         return "ssh {} {}".format(hop, cmd)
 
+    def __readSettings(self):
+        with open(QMFNetOp.SETTTINGS_FILE, 'r') as cfg:
+            log_cfg = yaml.safe_load(cfg)
+
+        ts = log_cfg.get('STATIONS')
+        if ts is not None:
+            self.ts = ts.split(',')
+        self.hop = log_cfg.get('hopStation')
+        
+
     def __init__(self):
         logging.basicConfig(level=logging.INFO)
         # logging.basicConfig(level=logging.DEBUG)
+        self.hop = None
+        self.ts = []
+        self.__readSettings()
 
 if __name__ == "__main__":
     pass
     # s= QMFNetOp().remote('find /RACKLOG/ -type f -name ZNH02200016.* ' )
-    QMFNetOp().querySn('B98340412317603B')
+    # QMFNetOp().querySn('B98340412317603B')
     # QMFNetOp().scp('192.168.0.81', '/RACKLOG/S2PL_PY/2020/Aug12/ZNH02200016/RUNIN/ZNH02200016.log')
+    q = QMFNetOp()
+    print(q.hop, q.ts)
